313d43bb800cb8cf965bec5b0440ef14a90d613a
Fernando RL <rlfernando7@gmail.com>
2025-04-12 00:15:53 -0600

    refactor: mover validación de email y contraseña al controlador de login

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,15 +1,33 @@
 import { request, response } from "express";
 import userModel from "../models/userModel.js";
 import generateJWT from "../helpers/generateJWT.js";
+import bcrypt from 'bcryptjs';
 
 const login = async (req = request, res = response) => {
 
     try {
 
-        const email = req.body.email;
+        const {email,password} = req.body;
         const user = await userModel.findOne({ email })
 
+        //Validate email exist
+        if (!user) {
+            return res.status(400).json({
+                msg: 'Usuario / Contraseña no son correctos'
+            })
+        }
+
+        //Validate and compare password
+        const validatePassword = bcrypt.compareSync(password, user.password);
+
+        if (!validatePassword) {
+            return res.status(400).json({
+                msg: 'Usuario / Contraseña no son correctos'
+            })
+        }
+
         //Validate user is active. Verificar que no fue borrado
+
 
         //Generate JWT
         const token = await generateJWT(user.id)
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -2,7 +2,6 @@ import express from 'express'
 import { check } from 'express-validator';
 import validateFields from '../middlewares/validate-fields.js';
 import { login } from '../controllers/auth.controller.js';
-import { comparePassword, validEmailLoginNotExist } from '../helpers/db-validators.js';
 
 const router = express.Router();
 
@@ -10,10 +9,8 @@ const router = express.Router();
 router.post('/', [
     check('email', 'El email es obligatorio').notEmpty(),
     check('email', 'Escriba un email correcto').isEmail(),
-    check('email').custom(validEmailLoginNotExist),
     check('password', 'La contraseña es obligatoria').notEmpty(),
     check('password', 'La contraseña debe tener minimo 5 caracteres').isLength({min:5}),
-    check('password').custom((password, {req}) => comparePassword( password, req)),
     validateFields
 ], login)
 
