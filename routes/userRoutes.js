83722953c8870bd87bf5067ac423e7c758838e0a
FerchoRL <rlfernando7@gmail.com>
2025-08-26 21:26:27 -0600

    fix(user): enforce USER_ROLE by default in createUser API

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -43,9 +43,6 @@ router.post('/', [
     check('email', 'El correo no es valido').isEmail(),
     check('password','El password es obligatorio').notEmpty(),
     check('password','El password debe tener mas de 4 caracteres').isLength({min: 5}),
-    check('role','El rol es obligatorio').notEmpty(),
-    check('role').custom( (role) => validRole(role) ),//Es lo mismo que abajo debido a que el parametro se llama igual
-    // check('role').custom( validRole ),
     check('email').custom(validExistedEmail),
     validateFields
 ], addUser)
